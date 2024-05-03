d854ae0f0766f9cebe38af7c387d06b1c0ad7aba
serm-webFox <109290607+serm-webFox@users.noreply.github.com>
2024-05-03 10:20:47 +0800

    Add files via upload

---
+++ MultyLang/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Программа перевода текста</h1>
+    <TranslationComponent />
+  </div>
+</template>
+
+<script>
+import TranslationComponent from '/src/components/TranslationComponent.vue';
+
+export default {
+  components: {
+    TranslationComponent,
+  },
+};
+</script>
---
+++ MultyLang/src/components/TranslationComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <input type="text" v-model="inputText" placeholder="Введите текст" />
+    <button @click="translateText">Перевести</button>
+    <p>Результат: {{ translatedText }}</p>
+  </div>
+</template>
+
+<script>
+
+import { createI18n } from 'vue-i18n';
+  
+const i18n = createI18n({
+  legacy: false,
+  locale: 'ru',
+  messages: {
+    ru: {
+      greeting: 'Привет, мир!',
+    },
+    en: {
+      greeting: 'Hello, world!',
+    },
+  },
+});
+
+export default {
+  data() {
+    return {
+      inputText: '',
+      translatedText: '',
+    };
+  },
+  methods: {
+    translateText() {
+      if (i18n.global.locale === 'ru') {
+        this.translatedText = i18n.global.t('en.greeting');
+      } else {
+        this.translatedText = i18n.global.t('ru.greeting');
+      }
+    },
+  },
+};
+</script>
